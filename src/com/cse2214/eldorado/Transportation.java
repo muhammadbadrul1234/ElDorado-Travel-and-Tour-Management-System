9436bd4c5e20799b1a80b7533487fba2122e2452
Badrul <badrulalomtawsyat@gmail.com>
2022-01-26 22:33:07 +0600

    commit

---
+++ src/com/cse2214/eldorado/Transportation.java
@@ -0,0 +1,28 @@
+package com.cse2214.eldorado;
+
+public class Transportation
+{
+    void DomesticFlight()
+    {
+
+        LogoAndUI logo = new LogoAndUI();
+        logo.Logo();
+
+        System.out.println("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*******************************************************************");
+        System.out.println("** BUS RESERVATION AND TICKETING SYSTEM   **");
+        System.out.println("********************************************");
+        System.out.println("** [1] Destination                   **");
+        System.out.println("** [2] Passengers                    **");
+        System.out.println("** [3] Billing                       **");
+        System.out.println("** [4] View                          **");
+        System.out.println("** [5] Exit                          **");
+        System.out.println("***************************************");
+        System.out.println("***************************************\n");
+
+
+
+    }
+
+
+
+}
